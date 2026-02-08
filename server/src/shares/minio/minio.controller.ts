f2557ce4709ebcef60b7b3e72e715be1bf842ae8
Karbbone <clement.maillet56@gmail.com>
2026-02-08 16:50:53 +0100

    fix : build

--- server/src/shares/minio/minio.controller.ts
+++ server/src/shares/minio/minio.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get, Req, Res, NotFoundException } from '@nestjs/common';
-import { Request, Response } from 'express';
+import type { Request, Response } from 'express';
 import { MinioService } from './minio.service';
 
 @Controller('files')
